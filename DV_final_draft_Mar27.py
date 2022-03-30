ac1e76c684ca698139034e6d5a2d25f2c3167a7d
ShwethaNS <shwetha.nagaraj.salimath@gmail.com>
2022-03-30 12:22:04 +0530

    Main chart
    Stacked donut chart

--- DV_final_draft_Mar27.py
+++ DV_final_draft_Mar27.py
@@ -8,6 +8,7 @@ import pandas as pd
 import altair as alt
 from altair import datum
 import plotly.express as px
+import plotly.graph_objs as go
 #from vega_datasets import data
 #from streamlit_vega_lite import vega_lite_component, altair_component
 import time
@@ -72,29 +73,55 @@ keys_1['options'] = options
 @st.cache(allow_output_mutation=True)
 def Main_chart(df, keys):
     # selected = alt.selection_multi(encodings=['x', 'y', 'size'])
-    source = df[df[keys['drug']] == 'CL0'][[keys['options'], 'ID']].groupby(keys['options']).count().transform(lambda x: 100*x/x.sum())
-    source_1 = df[df[keys['drug']] == 'CL1'][[keys['options'], 'ID']].groupby(keys['options']).count().transform(lambda x: 100*x/x.sum())
-    X = source_1.merge(source, on=keys['options'])
-    print(X)
-    source[keys['options']] = source.index
-    X[keys['options']] = X.index
+    source_1 = df[df[keys['drug']] == 'CL0'][[keys['options'], 'ID']].groupby(keys['options']).count().transform(
+        lambda x: 100 * x / x.sum())
+    source_2 = df[df[keys['drug']] == 'CL1'][[keys['options'], 'ID']].groupby(keys['options']).count().transform(
+        lambda x: 100 * x / x.sum())
+    X = source_1.merge(source_2, on=keys['options'])
+    source_3 = df[df[keys['drug']] == 'CL2'][[keys['options'], 'ID']].groupby(keys['options']).count().transform(
+        lambda x: 100 * x / x.sum())
+    X = X.merge(source_3, on=keys['options'])
 
-    chart1 = alt.Chart(X).mark_arc(innerRadius=200).encode(
-        theta=alt.Theta(field='ID_x', type="quantitative"),
-        color=alt.Color(field=keys['options'], type="nominal"),
-        tooltip=[keys['options'], 'ID_x']).properties(width=400, height=400)
-    chart2 = alt.Chart(X).mark_arc(innerRadius=100).encode(
-        theta=alt.Theta(field='ID_y', type="quantitative"),
-        color=alt.Color(field=keys['options'], type="nominal"),
-        tooltip=[keys['options'], 'ID_y']).properties(width=200, height=200)
-    chart = alt.layer(chart1, chart2)
-    return chart
+    data = [  # Portfolio (inner donut)
+        go.Pie(values=list(X['ID_x']),
+               labels=X.index.values.tolist(),
+               domain={'x': [0.3, 0.7], 'y': [0.2, 0.8]},
+               hole=0.5,
+               direction='clockwise',
+               sort=False,
+               marker=dict(colors=['#EC7063', '#F1948A', '#2E86C1', '#5DADE2', '#85C1E9'],
+                           line=dict(color='#000000', width=2))
+               ),
+        # Individual components (outer donut)
+        go.Pie(values=list(X['ID_y']),
+               labels=X.index.values.tolist(),
+               domain={'x': [0.2, 0.8], 'y': [0.1, 0.9]},
+               hole=0.75,
+               direction='clockwise',
+               sort=False,
+               marker=dict(colors=['#EC7063', '#F1948A', '#2E86C1', '#5DADE2', '#85C1E9'],
+                           line=dict(color='#000000', width=2)),
+               showlegend=False),
+        go.Pie(values=list(X['ID']),
+               labels=X.index.values.tolist(),
+               domain={'x': [0.1, 0.9], 'y': [0, 1]},
+               hole=0.75,
+               direction='clockwise',
+               sort=False,
+               marker=dict(colors=['#EC7063', '#F1948A', '#2E86C1', '#5DADE2', '#85C1E9'],
+                           line=dict(color='#000000', width=2)),
+               showlegend=False)
+    ]
+
+    fig = go.Figure(data=data, layout={'title': 'Percentage distribution of people in a category'})
+
+    return fig
 
 '''
 
 
 '''
-st.altair_chart(Main_chart(df, keys)) 
+st.plotly_chart(Main_chart(df, keys))
 
 # stacked pie chart: https://stackoverflow.com/questions/33019879/hierarchic-pie-donut-chart-from-pandas-dataframe-using-bokeh-or-matplotlib/33046810
 # nested pie chart : https://matplotlib.org/stable/gallery/pie_and_polar_charts/nested_pie.html#sphx-glr-gallery-pie-and-polar-charts-nested-pie-py
